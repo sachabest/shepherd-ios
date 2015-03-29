99f78c28e3a5378c22f37de8fe8e86ed4dc8221b
Rohun Bansal <rohun@rohunbansal.com>
2015-03-29 18:49:51 -0400

    sign up working

--- Shepherd/ChiefComplaintViewController.swift
+++ Shepherd/ChiefComplaintViewController.swift
@@ -231,4 +231,11 @@ extension ChiefComplaintViewController : PFLogInViewControllerDelegate {
     func logInViewControllerDidCancelLogIn(controller: PFLogInViewController) -> Void {
         // NOOP
     }
+    func signUpViewController(signUpController: PFSignUpViewController, didSignUpUser user: PFUser) -> Void {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func signUpViewControllerDidCancelSignUp(signUpController: PFSignUpViewController) -> Void {
+        // NOOP
+    }
 }
